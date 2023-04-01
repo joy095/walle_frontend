291035abbb8686871ae26c7bd3de72394cabe895
Joy Karmakar <96533682+joy095@users.noreply.github.com>
2023-04-01 16:40:11 +0530

    redme updated

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -24,15 +24,12 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch(
-        "https://walle-backend.onrender.com/api/v1/post",
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+      const response = await fetch("http://localhost:4000/api/v1/post", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
       if (response.ok) {
         const result = await response.json();
